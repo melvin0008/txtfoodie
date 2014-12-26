a9d9dd73bcb7e567d5e320b8635b979b7d394c90
Melvin <melvin0008@gmail.com>
2014-12-27 00:21:25 +0530

    testing heroku twilio

--- app.rb
+++ app.rb
@@ -15,13 +15,15 @@ get '/receive' do
 	message=params[:Body].split(' ')
 	if message.length ==1
 		foodquery=message[0]
-		"""@client.messages.create(
-			from:ENV['MY_NUMBER'],
-			to:params[:From],
-			body:'Yalla'
-			)"""
-		puts foodquery
+	
+		return foodquery
 	end
 end
 
+
+# @client.messages.create(
+# 			from:ENV['MY_NUMBER'],
+# 			to:params[:From],
+# 			body:'Yalla')
 #curl -X POST https://api.twilio.com/2010-04-01/Accounts/AC7ce5e0e39730f8a683247bd5fcd50e29/Messages -d "To=+16179528889" -d "From=+15005550006" -d "Body=Hello" -u 'AC7ce5e0e39730f8a683247bd5fcd50e29:c2daecaf9d24322e41c1208006e41466'
+#curl -X POST https://txtfoodie.herokuapp.com/receive -d "Body='yalla'"
