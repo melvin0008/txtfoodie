9a5707c2872a19a028d7f7ab1901d948dadfcff1
Melvin <melvin0008@gmail.com>
2014-12-27 00:17:47 +0530

    testing twilio

--- app.rb
+++ app.rb
@@ -11,14 +11,17 @@ Twilio.configure do |config|
 end
 
 get '/receive' do
-	@client= Twilio::REST::Client.new
+	#@client= Twilio::REST::Client.new
 	message=params[:Body].split(' ')
 	if message.length ==1
 		foodquery=message[0]
-		@client.messages.create(
+		"""@client.messages.create(
 			from:ENV['MY_NUMBER'],
-			to:"+919049733143",
-			body:"Yalla"
-			)
+			to:params[:From],
+			body:'Yalla'
+			)"""
+		puts foodquery
 	end
 end
+
+#curl -X POST https://api.twilio.com/2010-04-01/Accounts/AC7ce5e0e39730f8a683247bd5fcd50e29/Messages -d "To=+16179528889" -d "From=+15005550006" -d "Body=Hello" -u 'AC7ce5e0e39730f8a683247bd5fcd50e29:c2daecaf9d24322e41c1208006e41466'
